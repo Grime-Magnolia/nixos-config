63e3926898ebff3a256ce5752952ad1ecc2de6b0
Skiddedtheroad <tygozantman@gmail.com>
2025-07-26 13:47:49 +0200

    added github:horriblename/nixrun-nvim

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
+    fcitx-virtualkeyboard-adapter.url = "github:horriblename/fcitx-virtualkeyboard-adapter"
   };
 
   outputs = { self, nixpkgs, flake-utils, hyprland, home-manager, unstable, ... }:
