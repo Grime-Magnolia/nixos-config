295a256f13c66b2bd927374d5f6ce1c11b3f68ff
Skiddedtheroad <tygozantman@gmail.com>
2025-08-27 22:22:10 +0200

    added basic general config and revamped the bmaxnix config

---
+++ bmaxnix/configuration.nix
@@ -0,0 +1,73 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  specialisation = {
+    "nomodeset".configuration = {
+      boot.kernelParams = config.boot.kernelParams ++ ["nomodeset"];
+    };
+  };
+  networking.hostName = "bmaxnix"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "intel" ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tygo = {
+    isNormalUser = true;
+    extraGroups = [ 
+      "wheel" # Enable ‘sudo’ for the user.
+      "networkmanager" # Enables a posibility to 
+      "video"
+    ]; 
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
+
--- general-conf.nix
+++ general-conf.nix
@@ -1,4 +1,18 @@
-{
+{pkgs,unstable,lib,input,config,...}:
+let
+  cfg = config.general;
+in {
   imports = [];
-  options = {};
+  options.general = {
+    enable = lib.mkEnableOption "enables all apps and settings normaly used";
+    
+  };
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = config.environment.systemPackages ++ (with pkgs; [
+      kitty
+    ]);
+    programs.fish.enable = true;
+    users.defaultUserShell = pkgs.fish;
+    system.copySystemConfiguration = true;
+  };
 }
