952b09e85ebf7569ab6ee765991769d9de49f8c0
Skiddedtheroad <tygozantman@gmail.com>
2025-09-24 10:31:01 +0200

    added switch.sh

---
+++ homes/tygo/home.nix
@@ -0,0 +1,23 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  
+  wayland.windowManager.hyprland = {
+    enable = true ;
+    settings = {
+        source = "initializer.conf";
+      };
+    plugins = [
+      pkgs.hyprlandPlugins.hyprgrass
+    ];
+  };
+  home.stateVersion = "24.11";
+}
