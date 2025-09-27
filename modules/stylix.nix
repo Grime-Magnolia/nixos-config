e55dea567954c7a5bc3a75425fe3de19f1148a23
Skiddedtheroad <tygozantman@gmail.com>
2025-09-27 12:55:22 +0200

    moved stylix to its own module

---
+++ modules/stylix.nix
@@ -0,0 +1,10 @@
+{lib,pkgs,config, inputs, unstable, ...}:
+{
+  config = {
+      stylix = {
+        enable = true;
+        image = ../../wallpapers/orange_landscape.jpg;
+        polarity = "dark";
+    };
+  };
+}
