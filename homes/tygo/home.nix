417121b3e2c98a5f9b04f81c42ccd70e993052b0
Skiddedtheroad <tygozantman@gmail.com>
2025-09-27 11:23:02 +0200

    added waybar config to homemanager

--- homes/tygo/home.nix
+++ homes/tygo/home.nix
@@ -9,7 +9,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  
+  xdg.configFile."waybar/config.jsonc".source = ./waybar/config.jsonc;
   wayland.windowManager.hyprland = {
     enable = true ;
     settings = {
