1b61e9793f5a2b3fa65b6b027129706c74405264
Skiddedtheroad <tygozantman@gmail.com>
2025-10-29 09:35:32 +0100

    updated shit

--- overlays/yt-dlp.nix
+++ overlays/yt-dlp.nix
@@ -1,13 +1,13 @@
 self: super: {
   yt-dlp = super.yt-dlp.overrideAttrs (oldAttrs: rec {
     pname = "yt-dlp";
-    version = "2025.09.26";
+    version = "2025.10.22";
 
     src = super.fetchFromGitHub {
       owner = "yt-dlp";
       repo = "yt-dlp";
       rev = version;
-      sha256 = "sha256-/uzs87Vw+aDNfIJVLOx3C8RyZvWLqjggmnjrOvUX1Eg=";
+      sha256 = "sha256-jQaENEflaF9HzY/EiMXIHgUehAJ3nnDT9IbaN6bDcac=";
     };
   });
 }
