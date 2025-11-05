4f29bcd1fa1b2a18a2f8b406f9e39d83dc3ec135
Skiddedtheroad <tygozantman@gmail.com>
2025-11-05 09:02:56 +0100

    updated and fixed flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-grub-themes.url = "github:jeslie0/nixos-grub-themes";
     stylix = {
       url = "github:nix-community/stylix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +17,7 @@
     impermanence.url = "github:nix-community/impermanence";
   };
   
-  outputs = inputs@{ self, nixpkgs, impermanence, stylix, nixos-hardware, flake-utils, hyprland, home-manager, unstable, ... }:
+  outputs = inputs@{ self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       nixosModules = {
@@ -24,7 +25,7 @@
       };
       customModules = builtins.attrValues self.nixosModules;
       withCustomModules = modules: modules ++ builtins.attrValues nixosModules;
-      pkgsFor = system: import nixpkgs {
+      pkgsFor = system: flake: import flake {
         inherit system;
         overlays = [
           (import ./overlays/yt-dlp.nix)
@@ -36,9 +37,11 @@
           "mbedtls-2.28.10"
         ];
       };
-      pkgs = pkgsFor system;
+      pkgs = pkgsFor system inputs.nixpkgs;
+      stable = pkgsFor system inputs.nixpkgs;
+      unstable = pkgsFor system inputs.unstable;
     in 
-      flake-utils.lib.eachDefaultSystem (system:       
+      inputs.flake-utils.lib.eachDefaultSystem (system:       
         {
           # For use with `nix develop` or `nix run`
           devShell = pkgs.mkShell {
@@ -58,20 +61,24 @@
               config.allowUnfree = true;
             };
           };
+          mergekit = pkgs.callPackage ./packages/mergekit/mergekit.nix {
+            latest = (pkgsFor system nixpkgs); inherit unstable;
+          };
         };
 
         nixosConfigurations = {
           nixtop = nixpkgs.lib.nixosSystem {
             inherit system;
             inherit pkgs;
+            specialArgs = { inherit inputs; inherit stable; inherit unstable;};
             modules = withCustomModules [
-              stylix.nixosModules.stylix
+              inputs.stylix.nixosModules.stylix
               ./modules/stylix.nix
               ./general-conf.nix
               ./modules/arrstack.nix
               ./machines/nixtop/configuration.nix
               ./machines/nixtop/hardware-configuration.nix
-              home-manager.nixosModules.home-manager
+              inputs.home-manager.nixosModules.home-manager
               {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
@@ -81,31 +88,23 @@
                   ];
                 };
                 home-manager.backupFileExtension = null;
-              }
-              {
-                _module.args = {
-                  inherit inputs;
-                  unstable = import unstable {
-                    system = "x86_64-linux";
-                    config.allowUnfree = true;
-                  };
-                };
               }
             ];
           };
           frametop = nixpkgs.lib.nixosSystem {
             inherit system;
             inherit pkgs;
+            specialArgs = { inherit inputs; inherit stable; inherit unstable;};
             modules = withCustomModules [
-              stylix.nixosModules.stylix
-              impermanence.nixosModules.impermanence
-              nixos-hardware.nixosModules.framework-amd-ai-300-series
+              inputs.stylix.nixosModules.stylix
+              inputs.impermanence.nixosModules.impermanence
+              inputs.nixos-hardware.nixosModules.framework-amd-ai-300-series
               ./modules/stylix.nix
               ./general-conf.nix
               ./modules/arrstack.nix
               ./machines/frametop/configuration.nix
               ./machines/frametop/hardware-configuration.nix
-              home-manager.nixosModules.home-manager
+              inputs.home-manager.nixosModules.home-manager
               {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
@@ -115,15 +114,6 @@
                   ];
                 };
                 home-manager.backupFileExtension = null;
-              }
-              {
-                _module.args = {
-                  inherit inputs;
-                  unstable = import unstable {
-                    system = "x86_64-linux";
-                    config.allowUnfree = true;
-                  };
-                };
               }
             ];
           };
@@ -131,26 +121,17 @@
             inherit system;
             inherit pkgs;
             modules = [
-              stylix.nixosModules.stylix
+              inputs.stylix.nixosModules.stylix
               ./general-conf.nix
               ./modules/arrstack.nix
               ./machines/tynix/configuration.nix
               ./machines/tynix/hardware-configuration.nix
-              home-manager.nixosModules.home-manager
+              inputs.home-manager.nixosModules.home-manager
               {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
                 home-manager.users.tygo = import ./homes/tygo/home.nix;
                 home-manager.backupFileExtension = null;
-              }
-              {
-                _module.args = {
-                  inherit inputs;
-                  unstable = import unstable {
-                    system = "x86_64-linux";
-                    config.allowUnfree = true;
-                  };
-                };
               }
             ];
           };
