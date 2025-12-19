95a107f944bdd9dd1483b4a48f2977aa7aee2ec7
Skiddedtheroad <tygozantman@gmail.com>
2025-12-19 22:07:38 +0100

    gave tuner privlages to get data

--- packages/tuner/tuner.nix
+++ packages/tuner/tuner.nix
@@ -2,22 +2,19 @@
 pkgs.runCommand "tuner" {
   __impure = true;
   __noChroot = true;
-  buildInputs = [ pkgs.powertop pkgs.gawk pkgs.coreutils ];
+  buildInputs = [ pkgs.powertop pkgs.gawk pkgs.sudo ];
 } ''
   mkdir -p $out
-  powertop --auto-tune-dump || {
-    echo "powertop failed:" >&2
-    powertop --auto-tune-dump >&2
-    exit 1
-  } | awk '
-    /### auto-tune-dump commands BEGIN/ {flag=1; next}
-    /### auto-tune-dump commands END/   {flag=0; exit}
-    flag && $0 ~ /^# echo/ {
-      sub(/^# /, "")
-      print
-    }
-  ' > $out/tuner.sh
+  sudo powertop --auto-tune-dump 2>/dev/null \
+    | awk '
+      /### auto-tune-dump commands BEGIN/ {flag=1; next}
+      /### auto-tune-dump commands END/   {flag=0; exit}
+      flag && $0 ~ /^# echo/ {
+        sub(/^# /, "")
+        print
+      }
+    ' > $out/tuner.sh
   chmod +x $out/tuner.sh
-  cat $out/tuner.sh  # verify output
+  cat $out/tuner.sh
 ''
 
