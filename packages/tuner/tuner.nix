2d791d227396e22c8989ba7afdd09c9007ed7d5d
Skiddedtheroad <tygozantman@gmail.com>
2025-12-19 22:05:46 +0100

    updated tuner

--- packages/tuner/tuner.nix
+++ packages/tuner/tuner.nix
@@ -1,19 +1,23 @@
 { pkgs, ... }:
 pkgs.runCommand "tuner" {
-  __impure = true;  # This is the key
-  __noChroot = true; # removes sandboxing YES THIS IS DANGEROUS
-  buildInputs = [ pkgs.powertop pkgs.gawk ];
+  __impure = true;
+  __noChroot = true;
+  buildInputs = [ pkgs.powertop pkgs.gawk pkgs.coreutils ];
 } ''
   mkdir -p $out
-  powertop --auto-tune-dump 2>/dev/null \
-    | awk '
-      /### auto-tune-dump commands BEGIN/ {flag=1; next}
-      /### auto-tune-dump commands END/   {flag=0; exit}
-      flag && $0 ~ /^# echo/ {
-        sub(/^# /, "")
-        print
-      }
-    ' > $out/tuner.sh
+  powertop --auto-tune-dump || {
+    echo "powertop failed:" >&2
+    powertop --auto-tune-dump >&2
+    exit 1
+  } | awk '
+    /### auto-tune-dump commands BEGIN/ {flag=1; next}
+    /### auto-tune-dump commands END/   {flag=0; exit}
+    flag && $0 ~ /^# echo/ {
+      sub(/^# /, "")
+      print
+    }
+  ' > $out/tuner.sh
   chmod +x $out/tuner.sh
+  cat $out/tuner.sh  # verify output
 ''
 
